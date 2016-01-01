3d4b982beb0e6c3fcc866954d99fa2b77923791f
Dominik Stadler <dominik.stadler@gmx.at>
2016-01-01 16:25:59 +0100

    Fix unit test

--- src/test/java/org/dstadler/commoncrawl/oldindex/ProcessAndDownloadTest.java
+++ src/test/java/org/dstadler/commoncrawl/oldindex/ProcessAndDownloadTest.java
@@ -18,23 +18,23 @@ public class ProcessAndDownloadTest {
     	assertTrue(tempDir.delete());
     	File prevDir = Utils.DOWNLOAD_DIR;
     	Utils.DOWNLOAD_DIR = tempDir;
-    	
-        File destFile = new File(tempDir, "11.123.196.205_e99ca2lsdqfg_2sf4659mhyqd2dy_Word+Study+Handout.doc");
+
+        File destFile = new File(tempDir, "11.123.196.205_iraz4x2bg0xg_2sf4659mhyqd2dy_Word_Study_Handout.doc");
         try {
 	        File file = File.createTempFile("ProcessAndDownloadTest", ".tmp");
 	        try {
 	            assertTrue(file.delete());
-	            
+
 	            try (BlockProcessor process = new ProcessAndDownload(file, false)) {
 	                byte[] block = FileUtils.readFileToByteArray(new File("src/test/data/block1.bin"));
-	                
+
 	                process.offer(block, 0);
 	            } // the implicit close() will wait for the queue to be empty...
-	
+
 	            // need to do this outside the block to let process.close() join the Thread
 	            assertTrue(file.exists());
 	            assertTrue(file.length() > 0);
-	            
+
 				assertTrue("Should have resulting file, but did not find it at " + destFile, destFile.exists());
 	        } finally {
 	            assertTrue(file.delete());
