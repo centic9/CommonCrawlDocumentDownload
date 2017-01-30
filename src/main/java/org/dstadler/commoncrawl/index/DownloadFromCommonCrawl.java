f309077371eda6c7eabb4f4051516a3d1e20d986
Dominik Stadler <dominik.stadler@gmx.at>
2017-01-30 23:56:14 +0100

    Switch to CC-MAIN-2016-50

--- src/main/java/org/dstadler/commoncrawl/index/DownloadFromCommonCrawl.java
+++ src/main/java/org/dstadler/commoncrawl/index/DownloadFromCommonCrawl.java
@@ -20,7 +20,7 @@ import org.dstadler.commons.logging.jdk.LoggerFactory;
 public class DownloadFromCommonCrawl {
 	private static final Logger log = LoggerFactory.make();
 
-	private static final File COMMONCRAWL_FILE = new File("commoncrawl-CC-MAIN-2016-44.txt");
+	private static final File COMMONCRAWL_FILE = new File("commoncrawl-CC-MAIN-2016-50.txt");
 
     public static void main(String[] args) throws Exception {
         Utils.ensureDownloadDir();
