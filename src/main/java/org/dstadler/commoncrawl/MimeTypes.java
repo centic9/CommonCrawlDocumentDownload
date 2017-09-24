2e1401b1087f847eaec740cc24da78a7b40937f1
Dominik Stadler <dominik.stadler@gmx.at>
2017-09-24 19:55:31 +0200

    Optimize performance of class MimeType as some simple profiling showed that a lot of time is spent in there Also use a fixed ordering to correctly detect some of the mimetypes as some patterns or more general than others.

--- src/main/java/org/dstadler/commoncrawl/MimeTypes.java
+++ src/main/java/org/dstadler/commoncrawl/MimeTypes.java
@@ -1,7 +1,9 @@
 package org.dstadler.commoncrawl;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 /**
@@ -11,70 +13,66 @@ import java.util.regex.Pattern;
  * @author dominik.stadler
  */
 public class MimeTypes {
-    private static final Map<Pattern, String> MIME_TYPES = new HashMap<>();
+    private static final List<Pair<Pattern, String>> MIME_TYPES = new ArrayList<>();
     static {
-    	// application/msword
-    	MIME_TYPES.put(Pattern.compile(".*msword.*"), ".doc");
-    	// application/vnd.ms-word
-    	MIME_TYPES.put(Pattern.compile(".*ms-word.*"), ".doc");
+        // NOTE: Order is important to have more specialized ones first
+
+    	// application/msword, application/vnd.ms-word
+    	MIME_TYPES.add(Pair.of(Pattern.compile(".*(?:msword|ms-word).*"), ".doc"));
     	// application/vnd.openxmlformats-officedocument.wordprocessingml.document
-    	MIME_TYPES.put(Pattern.compile(".*wordprocessingml.*"), ".docx");
+    	MIME_TYPES.add(Pair.of(Pattern.compile(".*wordprocessingml.*"), ".docx"));
 
-        // application/msexcel
-    	MIME_TYPES.put(Pattern.compile(".*msexcel.*"), ".xls");
-    	// application/vnd.ms-excel
-    	MIME_TYPES.put(Pattern.compile(".*ms-excel.*"), ".xls");
     	// application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
-    	MIME_TYPES.put(Pattern.compile(".*spreadsheetml.*"), ".xlsx");
+    	MIME_TYPES.add(Pair.of(Pattern.compile(".*spreadsheetml.*"), ".xlsx"));
     	// 
-    	MIME_TYPES.put(Pattern.compile("application/vnd.ms-excel.addin.macroEnabled.12"), ".xlsm");
+    	MIME_TYPES.add(Pair.of(Pattern.compile("application/vnd.ms-excel.addin.macroEnabled.12"), ".xlsm"));
     	// 
-    	MIME_TYPES.put(Pattern.compile("application/vnd.ms-excel.sheet.binary.macroEnabled.12"), ".xlsb");
+    	MIME_TYPES.add(Pair.of(Pattern.compile("application/vnd.ms-excel.sheet.binary.macroEnabled.12"), ".xlsb"));
+        // application/msexcel, application/vnd.ms-excel
+        MIME_TYPES.add(Pair.of(Pattern.compile(".*(?:msexcel|ms-excel).*"), ".xls"));
 
-        // application/vnd.ms-powerpoint
-        MIME_TYPES.put(Pattern.compile("application/vnd.ms-powerpoint.*"), ".ppt");
-    	// application/vnd.openxmlformats-officedocument.presentationml.presentation
-    	MIME_TYPES.put(Pattern.compile(".*presentationml.*"), ".pptx");
-        // application/vnd.ms-powerpoint.presentation.macroenabled.12
-        MIME_TYPES.put(Pattern.compile("application/vnd.ms-powerpoint.presentation.macroenabled.12"), ".pptm");
-        // application/vnd.ms-powerpoint.slideshow.macroenabled.12
-        MIME_TYPES.put(Pattern.compile("application/vnd.ms-powerpoint.slideshow.macroenabled.12"), ".ppsm");
         // application/vnd.openxmlformats-officedocument.presentationml.template
-        MIME_TYPES.put(Pattern.compile("application/vnd.openxmlformats-officedocument.presentationml.template"), ".potx");
+        MIME_TYPES.add(Pair.of(Pattern.compile("application/vnd.openxmlformats-officedocument.presentationml.template"), ".potx"));
+        // application/vnd.openxmlformats-officedocument.presentationml.presentation
+        MIME_TYPES.add(Pair.of(Pattern.compile(".*presentationml.*"), ".pptx"));
+        // application/vnd.ms-powerpoint.presentation.macroenabled.12
+        MIME_TYPES.add(Pair.of(Pattern.compile("application/vnd.ms-powerpoint.presentation.macroenabled.12"), ".pptm"));
+        // application/vnd.ms-powerpoint.slideshow.macroenabled.12
+        MIME_TYPES.add(Pair.of(Pattern.compile("application/vnd.ms-powerpoint.slideshow.macroenabled.12"), ".ppsm"));
+        // application/vnd.ms-powerpoint
+        MIME_TYPES.add(Pair.of(Pattern.compile("application/vnd.ms-powerpoint.*"), ".ppt"));
         // application/vnd.ms-officetheme
-        MIME_TYPES.put(Pattern.compile("application/vnd.ms-officetheme"), ".thmx");
+        MIME_TYPES.add(Pair.of(Pattern.compile("application/vnd.ms-officetheme"), ".thmx"));
 
         // application/vnd.ms-tnef
-    	MIME_TYPES.put(Pattern.compile(".*ms-tnef.*"), ".msg");
+    	MIME_TYPES.add(Pair.of(Pattern.compile(".*ms-tnef.*"), ".msg"));
     	
     	// application/vnd.openxmlformats-officedocument.drawingml.chart+xml
-    	MIME_TYPES.put(Pattern.compile(".*drawingml.*"), ".dwg");
-
-    	// application/vnd.openxmlformats-officedocument.vmlDrawing
-    	MIME_TYPES.put(Pattern.compile(".*vmlDrawing.*"), ".dwg");
+        // application/vnd.openxmlformats-officedocument.vmlDrawing
+    	MIME_TYPES.add(Pair.of(Pattern.compile(".*(?:drawingml|vmlDrawing).*"), ".dwg"));
     	
-    	// application/vnd.visio
-    	MIME_TYPES.put(Pattern.compile(".*visio.*"), ".vsd");
         // Visio 2013
-        MIME_TYPES.put(Pattern.compile("application/vnd.ms-visio.drawing.main+xml"), ".vsdx");
-        MIME_TYPES.put(Pattern.compile("application/vnd.ms-visio.template.main+xml"), ".vstx");
-        MIME_TYPES.put(Pattern.compile("application/vnd.ms-visio.stencil.main+xml"), ".vssx");
-        MIME_TYPES.put(Pattern.compile("application/vnd.ms-visio.drawing.macroEnabled.main\\+xml"), ".vsdm");
-        MIME_TYPES.put(Pattern.compile("application/vnd.ms-visio.template.macroEnabled.main\\+xml"), ".vstm");
-        MIME_TYPES.put(Pattern.compile("application/vnd.ms-visio.stencil.macroEnabled.main\\+xml"), ".vssm");
+        MIME_TYPES.add(Pair.of(Pattern.compile("application/vnd.ms-visio.drawing.main\\+xml"), ".vsdx"));
+        MIME_TYPES.add(Pair.of(Pattern.compile("application/vnd.ms-visio.template.main\\+xml"), ".vstx"));
+        MIME_TYPES.add(Pair.of(Pattern.compile("application/vnd.ms-visio.stencil.main\\+xml"), ".vssx"));
+        MIME_TYPES.add(Pair.of(Pattern.compile("application/vnd.ms-visio.drawing.macroEnabled.main\\+xml"), ".vsdm"));
+        MIME_TYPES.add(Pair.of(Pattern.compile("application/vnd.ms-visio.template.macroEnabled.main\\+xml"), ".vstm"));
+        MIME_TYPES.add(Pair.of(Pattern.compile("application/vnd.ms-visio.stencil.macroEnabled.main\\+xml"), ".vssm"));
+        // application/vnd.visio
+        MIME_TYPES.add(Pair.of(Pattern.compile(".*visio.*"), ".vsd"));
 
         // application/vnd.ms-outlook
-        MIME_TYPES.put(Pattern.compile("application/vnd.ms-outlook"), ".msg");
+        MIME_TYPES.add(Pair.of(Pattern.compile("application/vnd.ms-outlook"), ".msg"));
 
         // application/x-mspublisher
-        MIME_TYPES.put(Pattern.compile("application/x-mspublisher"), ".pub");
+        MIME_TYPES.add(Pair.of(Pattern.compile("application/x-mspublisher"), ".pub"));
 
-        MIME_TYPES.put(Pattern.compile("application/vnd.openxmlformats-officedocument"), ".ooxml");
-        MIME_TYPES.put(Pattern.compile("application/x-tika-ooxml"), ".ooxml");
-        MIME_TYPES.put(Pattern.compile("application/x-tika-ooxml-protected"), ".ooxml");
-        MIME_TYPES.put(Pattern.compile("application/x-tika-msoffice"), ".ole2");
+        MIME_TYPES.add(Pair.of(Pattern.compile("(?:application/vnd.openxmlformats-officedocument|" +
+                "application/x-tika-ooxml|" +
+                "application/x-tika-ooxml-protected)"), ".ooxml"));
+        MIME_TYPES.add(Pair.of(Pattern.compile("application/x-tika-msoffice"), ".ole2"));
 
-//    	MIME_TYPE_MATCHER.put(Pattern.compile(".*.*");
+//    	MIME_TYPE_MATCHER.put(Pattern.compile(".*.*"));
     	
     	/*
 vnd.openxmlformats-officedocument.custom-properties+xml 	application/vnd.openxmlformats-officedocument.custom-properties+xml 	
@@ -90,8 +88,8 @@ vnd.openxmlformats-package.relationships+xml 	application/vnd.openxmlformats-pac
     }
     
     public static boolean matches(String mime) {
-        for(Pattern pattern : MIME_TYPES.keySet()) {
-            if(pattern.matcher(mime).matches()) {
+        for(Pair<Pattern,String> entry : MIME_TYPES) {
+            if(entry.getKey().matcher(mime).matches()) {
                 return true;
             }
         }
@@ -100,11 +98,11 @@ vnd.openxmlformats-package.relationships+xml 	application/vnd.openxmlformats-pac
     }
     
     public static String toExtension(String mime) {
-    	if(mime == null) {
+    	if(mime == null || !mime.startsWith("application/")) {
     		return "";
     	}
 
-        for(Map.Entry<Pattern, String> entry : MIME_TYPES.entrySet()) {
+        for(Pair<Pattern, String> entry : MIME_TYPES) {
             if(entry.getKey().matcher(mime).matches()) {
             	return entry.getValue();
             }
